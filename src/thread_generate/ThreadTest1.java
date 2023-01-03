49a6978a10972de060032865cdd9e00aed47fac6
GoEun <w2219@e-mirim.hs.kr>
2023-01-03 14:36:50 +0900

    init commit

---
+++ src/thread_generate/ThreadTest1.java
@@ -0,0 +1,25 @@
+package thread_generate;
+
+public class ThreadTest1 extends Thread {
+	
+	@Override 
+	public void run() {
+		for(int i=0; i<100; i++) {
+			System.out.println("thread 실행"+(i+1));
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadTest1 t1 = new ThreadTest1();	//new born 상태
+		t1.start();
+
+	}
+
+}
